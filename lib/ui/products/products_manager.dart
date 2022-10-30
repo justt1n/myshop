5e019ff635da21c9e64ce5dc93d5ceb9f89c0965
justt1n <justt1n>
2022-10-30 20:06:32 +0700

    Cap nhat, xoa san pham tren Realtime Database cuar Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -57,7 +57,7 @@ class ProductsManager with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<Product?> addProduct(Product product) async {
+  Future<void> addProduct(Product product) async {
     final newProduct = await _productsService.addProduct(product);
     if (newProduct != null) {
       _items.add(newProduct);
@@ -65,11 +65,13 @@ class ProductsManager with ChangeNotifier {
     }
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
     if (index >= 0) {
-      _items[index] = product;
-      notifyListeners();
+      if (await _productsService.updateProduct(product)) {
+        _items[index] = product;
+        notifyListeners();
+      }
     }
   }
 
@@ -78,10 +80,16 @@ class ProductsManager with ChangeNotifier {
     product.isFavorite = !savedStatus;
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
     _items.removeAt(index);
     notifyListeners();
+
+    if (await _productsService.deleteProduct(id)) {
+      _items.insert(index, existingProduct);
+      notifyListeners();
+    }
   }
 
   int get itemCount {
